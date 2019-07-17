87d9140d15252001c5883e6c7c69da0294bb2b9a
unknown <henriquesombrio3@gmail.com>
2019-07-16 23:44:50 -0300

    Convertendo valores para real (R$)

---
+++ Formatando Valores/FrmPrincipal.dfm
@@ -0,0 +1,32 @@
+object Form2: TForm2
+  Left = 0
+  Top = 0
+  Caption = 'Form2'
+  ClientHeight = 201
+  ClientWidth = 447
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 8
+    Top = 64
+    Width = 75
+    Height = 25
+    Caption = 'Converter'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 8
+    Top = 16
+    Width = 121
+    Height = 21
+    TabOrder = 1
+  end
+end
---
+++ Formatando Valores/FrmPrincipal.pas
@@ -0,0 +1,34 @@
+unit FrmPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.Button1Click(Sender: TObject);
+var
+  Valor: Currency;
+begin
+  Valor := StrToCurr(Edit1.Text); //Vai pegar o que foi escrito no edit1 e vai converter en currency
+  Edit1.Text := FormatCurr('#,##0.00', Valor);
+end;
+end.
